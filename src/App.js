e831e2b8adb22f8fa2bebb0dc05d31332c11ddc5
Khadeeejah <wuraola81@gmail.com>
2021-12-23 18:25:33 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+ import React from "react";
+ import Navigation from "./components/Navigation";
+ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+ import routes from "./routes";
+ import { useStyles } from "./styles";
+ import Dashboard from "./pages/Dashboard";
+ 
+ const App = () => {
+   const classes = useStyles();
+   return (
+     <div className={classes.appRoot}>
+       <Router>
+         <Navigation />
+         <Dashboard />
+         <div>
+           <div className={classes.appBarSpacer}></div>
+           {/* <Switch>
+             {routes.map((route, index) => {
+               return (
+                 <Route exact key={index} path={route.path}>
+                   {route.component}
+                 </Route>
+               );
+             })}
+           </Switch> */}
+         </div>
+       </Router>
+     </div>
+   );
+ };
+ 
+ export default App;
+ 
---
+++ src/pages/Accounts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextField } from "@material-ui/core";
+
+const Accounts = () => {
+  return <div>
+    <h1>
+      Create New Payment
+    </h1>
+
+    
+  </div>;
+};
+
+export default Accounts;
